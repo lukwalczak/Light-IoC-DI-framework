9ecfc043df3af6a96268b645810392c038de1929
lukwalczak <73855867+lukwalczak@users.noreply.github.com>
2026-03-03 15:38:01 +0100

    :construction: chore: annotations for later use in DI / IoC implementation

--- src/main/java/io/github/lukwalczak1/framework/annotation/beans/Controller.java
+++ src/main/java/io/github/lukwalczak1/framework/annotation/beans/Controller.java
@@ -1,9 +1,12 @@
 package io.github.lukwalczak1.framework.annotation.beans;
 
+import io.github.lukwalczak1.framework.annotation.RequestMapping;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target(java.lang.annotation.ElementType.TYPE)
 public @interface Controller {
+    String value() default  "";
 }
