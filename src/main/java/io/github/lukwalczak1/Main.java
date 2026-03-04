53bd4f033d99b2d203277b37c07c716f0b09d058
lukwalczak <73855867+lukwalczak@users.noreply.github.com>
2026-03-04 21:37:05 +0100

    :sparkles: chore: Added main framework entrypoint

--- src/main/java/io/github/lukwalczak1/Main.java
+++ src/main/java/io/github/lukwalczak1/Main.java
@@ -1,15 +1,6 @@
 package io.github.lukwalczak1;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
 import io.github.lukwalczak1.framework.ApplicationContext;
-import io.github.lukwalczak1.framework.container.BeanFactory;
-import io.github.lukwalczak1.framework.web.DispatcherHandler;
-import io.github.lukwalczak1.framework.web.HttpServerManager;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) throws Exception{
