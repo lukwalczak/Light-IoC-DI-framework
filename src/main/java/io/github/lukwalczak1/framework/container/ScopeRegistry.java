d95a2b6c206727f5e1ea59f94cf5eb773d60da70
lukwalczak <73855867+lukwalczak@users.noreply.github.com>
2026-03-14 22:53:58 +0100

    :construction: chore: registry for all available scopes

---
+++ src/main/java/io/github/lukwalczak1/framework/container/ScopeRegistry.java
@@ -0,0 +1,32 @@
+package io.github.lukwalczak1.framework.container;
+
+import io.github.lukwalczak1.framework.scope.annotation.ApplicationScoped;
+import io.github.lukwalczak1.framework.scope.annotation.RequestScoped;
+import io.github.lukwalczak1.framework.scope.annotation.SessionScoped;
+import io.github.lukwalczak1.framework.scope.annotation.ViewScoped;
+import io.github.lukwalczak1.framework.scope.implementation.ApplicationScope;
+import io.github.lukwalczak1.framework.scope.implementation.RequestScope;
+import io.github.lukwalczak1.framework.scope.implementation.SessionScope;
+import io.github.lukwalczak1.framework.scope.implementation.ViewScope;
+import io.github.lukwalczak1.framework.scope.interfaces.Scope;
+
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScopeRegistry {
+
+    private Map<Class<? extends Annotation>, Scope> scopeMap = new HashMap<>();
+
+    public ScopeRegistry() {
+        scopeMap.put(ApplicationScoped.class, new ApplicationScope());
+        scopeMap.put(ViewScoped.class, new ViewScope());
+        scopeMap.put(RequestScoped.class, new RequestScope());
+        scopeMap.put(SessionScoped.class, new SessionScope());
+    }
+
+    public Scope getScope(Class<? extends Annotation> annotationClass) {
+        return scopeMap.get(annotationClass);
+    }
+
+}
